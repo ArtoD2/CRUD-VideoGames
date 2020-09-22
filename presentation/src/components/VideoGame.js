3b43894706c163e10385263846bc0dca73867a3c
artoD2 <artosekepyan@gmail.com>
2020-09-22 14:54:29 -0600

    additional changes to DAL

---
+++ presentation/src/components/VideoGame.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const VideoGame = (props) => {
+    return (
+        <div>
+            <h1>{props.game.name}</h1>
+        </div>
+    );
+};
+
+export default VideoGame;
+
+
